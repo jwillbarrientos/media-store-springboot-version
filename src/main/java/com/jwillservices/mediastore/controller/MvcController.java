360d0a0e1373c2c51eaf40f03221d27df4dc13e0
Jonathan Barrientos <barrientosjonah@gmail.com>
2026-02-10 10:37:56 -0300

    getprofilename is deleted, now thymeleaf is used instead to bring the name of the user to the welcome page

--- src/main/java/com/jwillservices/mediastore/controller/MvcController.java
+++ src/main/java/com/jwillservices/mediastore/controller/MvcController.java
@@ -1,8 +1,11 @@
 package com.jwillservices.mediastore.controller;
 
+import com.jwillservices.mediastore.entity.Client;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class MvcController {
@@ -12,7 +15,12 @@ public class MvcController {
     }
 
     @GetMapping("/welcome-page.html")
-    public String welcomePage(Model model) {
+    public String welcomePage(Model model, HttpSession session) {
+        Client client = (Client) session.getAttribute("client");
+        if (client == null) {
+            return "redirect:/";
+        }
+        model.addAttribute("email", client.getEmail());
         return "welcome-page";
     }
 }
