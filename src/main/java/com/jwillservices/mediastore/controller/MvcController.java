f38323c55469920ef8f4eb9fa21c5f6dd14ec202
Jonathan Barrientos <barrientosjonah@gmail.com>
2026-02-10 10:51:16 -0300

    loadtags is deleted, now thymeleaf is used instead to bring the tags of the user to the welcome page

--- src/main/java/com/jwillservices/mediastore/controller/MvcController.java
+++ src/main/java/com/jwillservices/mediastore/controller/MvcController.java
@@ -1,14 +1,24 @@
 package com.jwillservices.mediastore.controller;
 
 import com.jwillservices.mediastore.entity.Client;
+import com.jwillservices.mediastore.entity.Tag;
+import com.jwillservices.mediastore.service.TagService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.expression.Mvc;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class MvcController {
+    private final TagService tagService;
+
+    MvcController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
     @GetMapping("/")
     public String index() {
         return "index";
@@ -21,6 +31,8 @@ public class MvcController {
             return "redirect:/";
         }
         model.addAttribute("email", client.getEmail());
+        List<Tag> tags = tagService.getTagsByClientId(client.getId());
+        model.addAttribute("tags", tags);
         return "welcome-page";
     }
 }
